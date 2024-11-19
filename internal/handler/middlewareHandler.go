ad844f3ad8ff4dc91a909d1a34c6ce1662dcd48a
ExonegeS <2004gusak@gmail.com>
2024-11-20 04:34:59 +0500

    "Added CORS headers and preflight request handling to BasicAuthMiddleware, and updated REST test to include a new request for app.log file"

--- internal/handler/middlewareHandler.go
+++ internal/handler/middlewareHandler.go
@@ -9,8 +9,18 @@ import (
 )
 
 func BasicAuthMiddleware(next http.Handler) http.Handler {
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")                            // Allow all origins, replace "*" with specific origin if needed
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")          // Allow specific methods
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // Allow specific headers
+
+		// Handle preflight request
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		err := config.UpdateENV()
 		if err != nil {
 			utils.SendJSONError(w, http.StatusInternalServerError, err, "server-side error, contact admin to enable authorization")
